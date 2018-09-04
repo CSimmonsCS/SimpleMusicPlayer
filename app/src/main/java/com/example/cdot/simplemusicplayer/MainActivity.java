81e0fb2a5d9e37bcfd7d8a4b143e9556a701f748
Christian Simmons <csimmonscs@gmail.com>
2018-09-03 19:26:19 -0700

    First MusicPlayer Commit

---
+++ app/src/main/java/com/example/cdot/simplemusicplayer/MainActivity.java
@@ -0,0 +1,173 @@
+package com.example.cdot.simplemusicplayer;
+//CODED BY CHRISTIAN SIMMONS
+//IDE ANDROID STUDIO
+
+//This is a simple music player coded in the Android Studio environment
+//and in the Java coded language.  The music player contains the song "Cudi Montage"
+//by KIDSSEEGHOSTS.  The player has a progress scrubber,
+//volume adjuster, & play&pause button.  The song will play and stop
+//based on the play/pause button being pressed.  The volume adjuster will
+//amplify or quiet the song.  The progress scrubber shows
+//the current time of the song.
+
+import android.media.MediaPlayer;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button playBtn;
+    SeekBar positionBar;
+    SeekBar volumeBar;
+    TextView elapsedTimeLabel;
+    TextView remainingTimeLabel;
+    MediaPlayer mp;
+    int totalTime;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        playBtn = (Button) findViewById(R.id.playBtn);
+        elapsedTimeLabel = (TextView) findViewById(R.id.elapsedTimeLabel);
+        remainingTimeLabel = (TextView) findViewById(R.id.remainingTimeLabel);
+
+        //Media Player
+        mp = MediaPlayer.create(this, R.raw.cudimontage);
+        mp.setLooping(true);
+        mp.seekTo(0);
+        mp.setVolume(0.5f, 0.5f);
+        totalTime = mp.getDuration();
+
+        //Position Bar
+        positionBar = (SeekBar) findViewById(R.id.positionBar);
+        positionBar.setMax(totalTime);
+        positionBar.setOnSeekBarChangeListener(
+                new SeekBar.OnSeekBarChangeListener() {
+                    @Override
+                    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                        if (fromUser) {
+                            mp.seekTo(progress);
+                            positionBar.setProgress(progress);
+                        }
+                    }
+
+                    @Override
+                    public void onStartTrackingTouch(SeekBar seekBar) {
+
+                    }
+
+                    @Override
+                    public void onStopTrackingTouch(SeekBar seekBar) {
+
+                    }
+                }
+        );
+
+
+        //Volume Bar
+        volumeBar = (SeekBar) findViewById(R.id.volumeBar);
+        volumeBar.setOnSeekBarChangeListener(
+                new SeekBar.OnSeekBarChangeListener() {
+                    @Override
+                    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                        float volumeNum = progress / 100f;
+                        mp.setVolume(volumeNum, volumeNum);
+                    }
+
+                    @Override
+                    public void onStartTrackingTouch(SeekBar seekBar) {
+
+                    }
+
+                    @Override
+                    public void onStopTrackingTouch(SeekBar seekBar) {
+
+                    }
+                }
+
+        );
+
+        //Thread (Update positionBar & timeLabel)
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (mp != null) {
+                    try {
+                        Message msg = new Message();
+                        msg.what = mp.getCurrentPosition();
+                        handler.sendMessage(msg);
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                    }
+                }
+            }
+
+        }).start();
+
+    }
+
+
+    private Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            int currentPosition = msg.what;
+            //Updates positionBar
+            positionBar.setProgress(currentPosition);
+
+            //Update Labels
+            String elapsedTime = createTimeLabel(currentPosition);
+            elapsedTimeLabel.setText(elapsedTime);
+
+
+            String remainingTime = createTimeLabel(totalTime-currentPosition);
+            remainingTimeLabel.setText("- " + remainingTime);
+        }
+    };
+
+    public String createTimeLabel (int time) {
+        String timeLabel = "";
+        int min = time / 1000 / 60;
+        int sec = time / 1000 % 60;
+
+        timeLabel = min + ":";
+        if(sec<10) timeLabel += "0";
+        timeLabel += sec;
+
+        return timeLabel;
+
+    }
+
+
+    public void playBtnClick(View view){
+        if(!mp.isPlaying()){
+            //Stopping
+            mp.start();
+            playBtn.setBackgroundResource(R.drawable.stop);
+        }
+        else {
+            mp.pause();
+            playBtn.setBackgroundResource(R.drawable.play);
+        }
+    }
+
+}
+
+
+
+
+
+
+
+
